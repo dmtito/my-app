b5713c738dd766940ee3725072d0cf5ce1167cb0
medvedik <medvedikvitaly@gmail.com>
2024-05-30 19:05:04 +0200

    amqp added

--- customer/src/main/java/com/mycompany/customer/CustomerApplication.java
+++ customer/src/main/java/com/mycompany/customer/CustomerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+@EnableFeignClients(
+        basePackages = "com.mycompany.clients"
+)
 @SpringBootApplication
 public class CustomerApplication {
     public static void main(String[] args) {
